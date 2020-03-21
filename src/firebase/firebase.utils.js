0dbff9a71d17935aaacdde2831a6071e1e65e19e
Toxic254 <cyrus_g@outlook.com>
2020-03-21 22:13:05 +0300

    Worked on some Data Structures, hope they simulate perfect flow of data within components.

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -56,6 +56,24 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
     return await batch.commit();
 };
 
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollections = collections.docs.map(doc => {
+        const { title, items } = doc.data();
+        
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
+        }
+    });
+
+    return transformedCollections.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    }, {});
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
